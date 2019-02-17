8fbb4caf1e07b2a70f4216279b7d872eead5fd25
Kapustin Anton <kapustin.anton199712@gmail.com>
2019-02-17 17:23:11 +0700

    Start

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя");
+            string name;
+            name = Console.ReadLine();
+            if (name == "")
+                Console.WriteLine("Здраствуй мир!");
+            else
+                Console.WriteLine("Здраствуй, " + name + "!");
+            Console.ReadLine();
+        }
+    }
+}
