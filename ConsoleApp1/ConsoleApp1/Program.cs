ddda1a2cbf31d52b38f8698d23acfba34cf34437
Kapustin Anton <kapustin.anton199712@gmail.com>
2019-02-17 18:27:43 +0700

    Finish

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,16 +11,19 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Введите символы");
-            int i;
+            int i, a=0,b=0;
             do
             {
                 i = Console.Read();
                 if ((char)i == '#')
                     break;
+                if ((char)i == '0')
+                    a++;
+                if ((char)i == '1')
+                    b++;
                 
-                Console.Write((char)i);
             } while (true);
-            
+            Console.WriteLine("Символ 0 встречается " + a + ", 1 встерчается " + b + " раз.");
             Console.ReadLine();
             Console.ReadLine();
         }
